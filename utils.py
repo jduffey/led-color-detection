b77bea74dd4d13a5e85cef44a7c4549733ec3311
Jed Duffey <jedduffey@gmail.com>
2020-03-12 12:50:51 -0400

    minor refactor

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ IMAGE_DIRECTORY = 'images'
 NUM_CLUSTERS = 5
 
 
-def get_dominant_RGB_value(image):
+def get_dominant_RGB_values(image):
     im = Image.open(image)
     im = im.resize((150, 150))  # optional, to reduce time
     ar = np.asarray(im)
@@ -25,42 +25,32 @@ def get_dominant_RGB_value(image):
     hex_color = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
 
     h = hex_color
-    rgb = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+    r = int(h[0:2], 16)
+    g = int(h[2:4], 16)
+    b = int(h[4:6], 16)
 
-    return rgb
+    return r, g, b
 
 
 def is_red(image):
-    rgb = get_dominant_RGB_value(image)
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
+    r, g, b = get_dominant_RGB_values(image)
 
     return r > 160 and g < 100 and b < 140
 
 
 def is_green(image):
-    rgb = get_dominant_RGB_value(image)
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
-    
+    r, g, b = get_dominant_RGB_values(image)
+
     return r < 100 and g > 160 and b > 80
 
 
 def is_blue(image):
-    rgb = get_dominant_RGB_value(image)
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
-    
+    r, g, b = get_dominant_RGB_values(image)
+
     return r < 100 and g < 100 and b > 200
 
 
 def is_white(image):
-    rgb = get_dominant_RGB_value(image)
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
-    
+    r, g, b = get_dominant_RGB_values(image)
+
     return r > 180 and g > 180 and b > 180
